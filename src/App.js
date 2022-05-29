ef2723cf4f98b088e0a08a0b135b4f87a106516d
DipankarDDUT <deepankar.dutta1@gmail.com>
2022-05-29 22:29:37 +0530

    2 files modified, 4 added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import React from 'react';
 
 import './style.css';
-
-const Hello = ({ name, age }) => {
-  return (
-    <div>
-      <p>
-        Hello {name} and {age}
-      </p>
-    </div>
-  );
-};
+import Header from './Header.js';
+import Content from './Content.js';
 
 const App = () => {
-  const name = 'Dipankar';
-  const age = 28;
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name={name} age={age} />
-      <Hello name="Daisy" age={10 + 12} />
-      <Hello name="George" age={44 + 2} />
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      {/* <Total ... /> */}
     </div>
   );
 };
+
 export default App;
