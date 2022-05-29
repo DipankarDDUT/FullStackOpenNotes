51c1dd5a6894a97ee29480e726784747163effcf
DipankarDDUT <deepankar.dutta1@gmail.com>
2022-05-29 19:20:35 +0530

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+// import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
 import App from './App';
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
+// ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById('root'));
