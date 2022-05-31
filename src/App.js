41b190c8d3066fe1a514395e12b6c6582d25241c
DipankarDDUT <deepankar.dutta1@gmail.com>
2022-05-31 23:08:44 +0530

    2 files modified, 4 deleted

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
 import React from 'react';
-
-import './style.css';
-import Header from './Header.js';
-import Content from './Content.js';
-import Total from './Total.js';
+import { useState } from 'react';
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-      },
-    ],
-  };
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content
-        part1={course.parts[0]}
-        part2={course.parts[1]}
-        part3={course.parts[2]}
-      />
-      <Total course={course} />
-    </div>
-  );
+  const [counter, setCounter] = useState(0);
+
+  setTimeout(() => setCounter(counter + 1), 1000);
+
+  return <div>{counter}</div>;
 };
 
 export default App;
